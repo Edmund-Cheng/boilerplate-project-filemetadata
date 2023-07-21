184b2cbe902ec6bf9f9ff8e86fe13170df9a88c7
Edmund-Cheng <119930913+Edmund-Cheng@users.noreply.github.com>
2023-07-21 14:18:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,25 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// setup multer to handle file uploading
+const multer = require('multer');
+// use memoryStorage instead of diskStorage for exercise
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const fileName = req.file.originalname;
+  const fileType = req.file.mimetype;
+  const fileSize = req.file.size;
+  console.log(fileName, fileType, fileSize);
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+      });
+})
